b08201e5b55e82b34f90f179e9850a0801d195b5
luisfmcalado@gmail.com <luisfmcalado@gmail.com>
2014-11-03 11:26:20 +0000

    refactor

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 /* 
  * File:   main.c
- * Author: Luis Calado <sguergachi at gmail.com>
+ * Author: Luis Calado <luisfmcalado at gmail.com>
  *
  * Created on 2 de Outubro de 2014, 14:25
  */
@@ -10,53 +10,53 @@
 #include "../deps/sqlite3.h"
 #include <string.h>
 
+#ifdef _WIN32
+#define OS "Windows"
+#define PATH1 "C:\\Users\\"
+#define PATH2 "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\history"
+#define PATH1XP " C:\\Documents and Settings\\"
+#define PATH2XP "\\Local Settings\\Application Data\\Google\\Chrome\\User Data\\Default"
+#define QUERY "SELECT lower_term, term FROM keyword_search_terms"
+#elif __APPLE__
+#elif __linux
+#endif
+
+void getPath(char* str, const char* user, const char* part1, const char* part2) {
+    strcpy(str, part1);
+    strcat(str, user);
+    strcat(str, part2);
+}
+
 int main(int argc, char** argv) {
-    
+
     setvbuf(stdout, NULL, _IONBF, 0);
-    
-    char nome[255];
-    printf("Insira nome do utilizador: ");
-    scanf("%s", nome);
-    
-    //CONEXAO
+
     sqlite3 *db;
+    sqlite3_stmt *stmHandle;
+    char nome[255];
     char googleChrome[255];
-    char googleChromeXP[255];
+    char lowerterm[255];
+    char term[255];
+    int len = strlen(QUERY) + 1;
+    int hasRow;
 
-    strcpy(googleChrome, "C:\\Users\\");
-    strcat(googleChrome, nome);
-    strcat(googleChrome, "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History");
+    printf("Insert user name: ");
+    scanf("%s", nome);
 
-    strcpy(googleChromeXP, "C:\\users\\");
-    strcat(googleChromeXP, nome);
-    strcat(googleChromeXP, "\\Local Settings\\Application Data\\Google\\Chrome\\User Data\\Default\\History");
+    getPath(googleChrome, nome, PATH1, PATH2);
 
     if (sqlite3_open(googleChrome, &db) != SQLITE_OK) {
-        if (sqlite3_open(googleChromeXP, &db) != SQLITE_OK) {
-            fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-            exit(0);
-        }
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        exit(0);
     }
-    printf("Opened database successfully\n");
 
-    //PREPARACAO
-    const char *query = "SELECT lower_term, term FROM keyword_search_terms";
-    int len = strlen(query) + 1;
-    sqlite3_stmt *stmHandle;
-
-    if (sqlite3_prepare(db, query, len, &stmHandle, NULL) != SQLITE_OK) {
-        fprintf(stderr, "Não foi possivel fazer a query: %s\n", sqlite3_errmsg(db));
+    if (sqlite3_prepare(db, QUERY, len, &stmHandle, NULL) != SQLITE_OK) {
+        fprintf(stderr, "Couldn't execute query: %s\n", sqlite3_errmsg(db));
         sqlite3_close(db);
         exit(0);
     }
-    printf("Query realizada com sucesso.\n");
-
-    char *lowerterm = (char *) malloc(255);
-    char *term = (char *) malloc(255);
-    int hasRow;
-
+    
     do {
-
         hasRow = sqlite3_step(stmHandle);
 
         if (hasRow == SQLITE_ROW) {
@@ -64,20 +64,16 @@ int main(int argc, char** argv) {
             strcpy(term, (char *) sqlite3_column_text(stmHandle, 1));
         }
 
-        printf("\tLower Termo: %s -::- Termo: %s \n", lowerterm, term);
+        printf("Lower Termo: %s -::- Termo: %s \n", lowerterm, term);
 
     } while (hasRow == SQLITE_ROW);
-
-    free(lowerterm);
-    free(term);
     sqlite3_close(db);
 
     printf("Prima <enter> para sair.\n");
-    
     int ch;
-    while((ch = getchar()) != '\n');
+    while ((ch = getchar()) != '\n');
     getchar();
-    
+
     return (EXIT_SUCCESS);
 }
 
